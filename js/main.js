eb313ffdd0a7755061f4429bfb643d0244b80d33
kennee10 <kennee100@gmail.com>
2021-06-15 13:46:01 +0800

    color variables

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+// ==== preloader ====
+$(window).on('load',function(){
+    $(".loader-wrapper").fadeOut(1250);
+});
+
+// ==== theme toggle btn ====
 document.querySelector(".theme-toggle-btn").addEventListener
 ("click", () => {
     document.body.classList.toggle('dark');
-})
+});
